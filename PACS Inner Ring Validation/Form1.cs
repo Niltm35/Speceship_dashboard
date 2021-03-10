9a2526a70db62f8cb31031813c048d83063d7c52
Niltm35 <72029272+Niltm35@users.noreply.github.com>
2021-03-10 15:57:32 +0100

    branch nil

--- PACS Inner Ring Validation/Form1.cs
+++ PACS Inner Ring Validation/Form1.cs
@@ -131,5 +131,315 @@ namespace PACS_Inner_Ring_Validation
                 this.DialogResult = DialogResult.OK;
             }
         }
+<<<<<<< Updated upstream
+=======
+        
+        Thread fil_codificacion_letra;
+        Thread fil_codificacion_num;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int h = 0;
+            fil_codificacion_letra = new Thread(generacion_cod_fit);
+            fil_codificacion_num = new Thread(generacio_cod_fit_num);
+            fil_codificacion_letra.Start();
+            fil_codificacion_num.Start();
+        }
+        
+
+        private void generacion_cod_fit()
+        {
+            var numero = new Random();
+
+            int aleatori;
+            for (char j = 'A'; j <= 'Z'; j++)
+            {
+                aleatori = numero.Next(0, 999);
+                string numero_aleatorio = "";
+                if (aleatori.ToString().Length == 2)
+                {
+                    numero_aleatorio = 0 + aleatori.ToString();
+                }
+                else if(aleatori.ToString().Length == 1)
+                {
+                    numero_aleatorio = 00 + aleatori.ToString();
+                }
+                else
+                {
+                    numero_aleatorio = aleatori.ToString();
+                }
+
+                diccionari.Add(j.ToString(), numero_aleatorio);
+            }
+
+            Parallel.For(1, 4, i =>
+            {
+                string filepath = @"Fitxers\Lletres\" + "PACS" + i + ".txt";
+                FileStream fs = new FileStream(filepath, FileMode.Create, FileAccess.Write);
+                StreamWriter writer = new StreamWriter(fs);
+                string lletra = "";
+            
+                RNGCryptoServiceProvider provider = new RNGCryptoServiceProvider();
+                var byteArray = new byte[4];
+
+
+                for (int u = 0; u < 100000; u++)
+                {
+                    provider.GetBytes(byteArray);
+                    var randomInteger = BitConverter.ToInt32(byteArray, 0);
+                    Random rnd = new Random(randomInteger);
+                    int posicio_abc = rnd.Next(0,26);
+                    lletra = lletra + diccionari.ElementAt((posicio_abc)).Key; 
+                }
+
+                writer.WriteLine(lletra);
+                writer.Flush();
+                writer.Close();
+            });
+            fil_codificacion_letra.Abort();
+        }
+        private void generacio_cod_fit_num()
+        {
+            fil_codificacion_letra.Join();
+
+            Parallel.For(1, 4, p =>
+            {
+                string filepath = @"Fitxers\Lletres\" + "PACS" + p + ".txt";
+                FileStream lletra_fit = new FileStream(filepath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+                StreamReader reader = new StreamReader(lletra_fit);
+
+                string filepath_numeros = @"Fitxers\Numeros\" + "PACS_num" + p + ".txt";
+                FileStream fs = new FileStream(filepath_numeros, FileMode.Create, FileAccess.Write, FileShare.Read);
+                StreamWriter writer = new StreamWriter(fs);
+                string numeros = "";
+                string lletra = "";
+                bool end = false;
+
+                while (!end)
+                {
+                    lletra = Convert.ToString((char)reader.Read());
+                    if (lletra == "\r")
+                    {
+                        end = true;
+                        writer.WriteLine();
+                    }
+                    else
+                    {
+                        writer.Write(diccionari[lletra]);
+                    }
+                }
+                
+                writer.Flush();
+                writer.Close();
+            });
+
+
+            var zipFile = Application.StartupPath + "\\Fitxers\\PACS.zip";
+            var files = Directory.GetFiles(@"Fitxers\Lletres");
+
+            if (File.Exists(zipFile))
+            {
+                File.Delete(zipFile);
+            }
+            using (var archive = ZipFile.Open(zipFile, ZipArchiveMode.Create))
+            {
+                foreach (var fPath in files)
+                {
+                    archive.CreateEntryFromFile(fPath, Path.GetFileName(fPath));
+                }
+            }
+
+            fil_codificacion_letra.Abort();
+        }
+
+        //-----------------CLIENT--------------------------------
+        Thread Fil_ping;
+        string IP_Planet;
+        string Port_Planet;
+
+        public DataSet dd()
+        {
+            string SELECT = "SELECT * FROM Planets where idPlanet = " + comboPlanets.SelectedValue;
+            string TAULA = "Planets";
+            DataSet Data_Planet = bbdd.PortarPerConsulta(SELECT, TAULA);
+            return Data_Planet;
+        }
+        private void comboPlanets_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            IP_Planet = dd().Tables[0].Rows[0][10].ToString();
+            Port_Planet = dd().Tables[0].Rows[0][11].ToString();
+            //progressBar1.Value = 100;
+        }
+
+        private void ping()
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            btn_comprovarXarxa.Enabled = false;
+            btn_desconnect.Enabled = false;
+            Boolean xarxaDisponible;
+            xarxaDisponible = NetworkInterface.GetIsNetworkAvailable();
+
+            if (xarxaDisponible)
+            {
+                pnl_status.BackColor = Color.Yellow;
+
+                Ping myPing = new Ping();
+
+                PingReply reply;
+
+                string Estado = "";
+                for (int i = 1; i <= 10; i++)
+                {
+                    reply = myPing.Send(IP_Planet, 8888);
+
+
+                    if (reply.Address != null)
+                    {
+                        Estado = "OK";
+                        //Thread.Sleep(10);
+                    }
+                    else
+                    {
+                        Estado = "NOK";
+                    }
+                    lbx_console.Items.Add("Ping" + i + " - " + Estado);
+                    
+                }
+                if (Estado.Equals("NOK"))
+                {
+                    pnl_status.BackColor = Color.Red;
+                    btn_comprovarXarxa.Enabled = true;
+                    btn_desconnect.Enabled = false;
+                    lb_statusInfo.Text = "Ping no contesta o Xarxa no disponible";
+                }
+                else
+                {
+                    pnl_status.BackColor = Color.Green;
+                    btn_comprovarXarxa.Enabled = true;
+                    btn_desconnect.Enabled = true;
+                    btn_sendMessage.Enabled = true;
+                    lb_statusInfo.Text = "Connexió correcta";
+                }
+            }
+            else
+            {
+                pnl_status.BackColor = Color.Red;
+            }
+        }
+        private void btn_comprovarXarxa_Click(object sender, EventArgs e)
+        {
+            lbx_console.Items.Clear();
+
+            Fil_ping = new Thread(ping);
+            Fil_ping.Start();
+        }
+
+        private void btn_sendMessage_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                TcpClient client = new TcpClient(IP_Planet, Convert.ToInt32(Port_Planet));
+                if (txb_message.Text == "")
+                {
+                    MessageBox.Show("El missatge no por estar buit");
+                }
+                else
+                {
+                    Byte[] dades = Encoding.ASCII.GetBytes(txb_message.Text);
+                    NetworkStream ns = client.GetStream();
+                    ns.Write(dades, 0, dades.Length);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Servidor inaccessible");
+            }
+        }
+
+        private void btn_desconnect_Click(object sender, EventArgs e)
+        {
+            cerrar();
+        }
+
+        //-----------------FIN CLIENT--------------------------------
+        //-----------------SERVIDOR--------------------------------
+
+        Thread comprobacio_conexio;
+
+        private void btn_connect_serv_Click(object sender, EventArgs e)
+        {
+            if (!IsConnected)
+            {
+                comprobacio_conexio = new Thread(conexio);
+                comprobacio_conexio.Start();
+                IsConnected = true;
+            }
+
+        }
+        Boolean IsConnected;
+        TcpClient client;
+        TcpListener Listener = null;
+        NetworkStream ns;
+
+        public void conexio()
+        {
+            try
+            {
+                Listener = new TcpListener(IPAddress.Any, Convert.ToInt32(Port_Planet));
+                Listener.Start();
+
+                while (IsConnected)
+                {
+                    if (Listener.Pending())
+                    {
+                        client = Listener.AcceptTcpClient();
+                        ns = client.GetStream();
+                        Byte[] buffer = new byte[256];
+
+                        string data = "";
+                        ns.Read(buffer, 0, buffer.Length);
+                        data = Encoding.ASCII.GetString(buffer, 0, buffer.Length);
+                        lbx_Missatges.Items.Add("IP: " + "la Nau" + " ha enviat: " + data);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btn_desconnect_serv_Click(object sender, EventArgs e)
+        {
+            lbx_Missatges.Items.Clear();
+            cerrar();
+        }
+
+        private void frmServer_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            cerrar();
+        }
+
+        public void cerrar()
+        {
+            IsConnected = false;
+            if (this.comprobacio_conexio != null)
+            {
+                comprobacio_conexio.Abort();
+            }
+            if (this.Listener != null)
+            {
+                Listener.Stop();
+            }
+            if (this.client != null)
+            {
+                client.Close();
+            }
+            if (this.ns != null)
+            {
+                ns.Close();
+            }
+        }
+>>>>>>> Stashed changes
     }
 }
